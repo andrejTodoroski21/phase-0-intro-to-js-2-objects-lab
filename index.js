f28bdd55ba0db89337e27758199251e3624bbde4
Andrej <andrejtodoroski@yahoo.com>
2024-02-23 19:09:30 -0500

    destructivly and undestructivly updates employee

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "andrej",
+    address: 123
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    const newobj = {...obj}
+    newobj[key] = value;
+    return newobj
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key]= value;
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newemployee = {...employee}
+    delete newemployee[key];
+    return newemployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
